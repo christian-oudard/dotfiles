8d00e9ed493438f672350b68dfa0e104ce69c0a8
Christian Oudard <ChristianOudard@pm.me>
2022-12-21 19:12:03 -0600

    Add key bindings for LSP. Add vim-fugitive for Git access within NeoVim.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ return require('packer').startup(function(use)
     use 'morhetz/gruvbox'
     use 'tpope/vim-commentary'
     use 'tpope/vim-surround'
+    use 'tpope/vim-fugitive'
     use 'SirVer/ultisnips'
     use 'ap/vim-buftabline'
     use '907th/vim-auto-save'
