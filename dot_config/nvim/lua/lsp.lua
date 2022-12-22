dfc500fefd84f4799a2e19617b7fdd6eca865b5a
Christian Oudard <ChristianOudard@pm.me>
2022-12-21 19:12:03 -0600

    Add key bindings for LSP. Add vim-fugitive for Git access within NeoVim.

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,1 +1,19 @@
-require'lspconfig'.rust_analyzer.setup{}
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+require'lspconfig'.rust_analyzer.setup{
+    on_attach = on_attach,
+}
+require'lspconfig'.pyright.setup{
+    on_attach = on_attach,
+}
