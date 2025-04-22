c82b0ef9e099f0c12561fffdeeab5277efcd72ae
Christian Oudard <ChristianOudard@pm.me>
2025-04-22 15:06:38 -0600

    Replace buftabline with bufferline.nvim.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,22 +1,27 @@
-return require('packer').startup(function(use)
+return require('packer').startup(
+  function(use)
     use 'wbthomason/packer.nvim'
     use 'morhetz/gruvbox'
     use 'tpope/vim-commentary'
     use 'tpope/vim-surround'
     use 'tpope/vim-fugitive'
     use 'SirVer/ultisnips'
-    use 'ap/vim-buftabline'
     use '907th/vim-auto-save'
     use 'neovim/nvim-lspconfig'
     use 'Vimjas/vim-python-pep8-indent'
     use 'github/copilot.vim'
     use {
-           "folke/trouble.nvim",
-           requires = "kyazdani42/nvim-web-devicons",
-           config = function() require("trouble").setup {} end
+      'folke/trouble.nvim',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() require('trouble').setup {} end
     }
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      requires = 'nvim-lua/plenary.nvim'
     }
-end)
+    use {
+      'akinsho/bufferline.nvim', tag = '*',
+      requires = 'nvim-tree/nvim-web-devicons',
+    }
+  end
+)
