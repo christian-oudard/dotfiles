55cd2c9b371b0857b28a479c1b3d31e328ba6989
Christian Oudard <ChristianOudard@pm.me>
2026-03-02 12:49:22 -0700

    Add PAM integration for gnome-keyring with greetd.

--- nixos-config/common.nix
+++ nixos-config/common.nix
@@ -74,6 +74,7 @@
 
   # Keyring (Secret Service API for Python keyring, etc.)
   services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
 
   # Auto-login and start Sway via greetd
   services.greetd = {
